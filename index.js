75576291cebddb5096091b4c373eacde73d635ee
beatriz.silva <beatriz.silva@tre-ms.jus.br>
2026-03-31 16:31:39 -0400

    conectando o express com o banco

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 import express from 'express';
-
-const app = express();
+import admin from 'firebase-admin';
 
 // REST API http://api.controle-gastos.com/transactions
+const app = express();
+
+admin.initializeApp({
+  credential: admin.credential.cert("./serviceAccountKey.json")
+});
+
 
 // GET      http://api.controle-gastos.com/transactions
 app.get('/transactions', (request, response) => 
 {
     console.log("GET transactions");
-    response.json([{id: 1}]);
+    admin.firestore()
+        .collection('transactions')
+        .get()
+        .then(snapshot => {
+           const transactions = snapshot.docs.map(doc => ({
+               ...doc.data(),
+                uid: doc.id
+            }))
+            response.json(transactions);
+         })
 })
-
-// GET      http://api.controle-gastos.com/transactions/:id
-// POST     http://api.controle-gastos.com/transactions
-// PUT      http://api.controle-gastos.com/transactions/:id
-// DELETE   http://api.controle-gastos.com/transactions/:id
 
 app.listen(3000, () => console.log('API rest iniciada em http://localhost:3000'))
 
